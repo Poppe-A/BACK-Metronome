eddce196a2decb017aa857d4d28ef57c1361765d
Antoine <antoinepoppe@hotmail.fr>
2021-03-30 15:25:48 +0200

    working API

--- app.js
+++ app.js
@@ -4,55 +4,75 @@ var express = require('express')
 var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
 
 // Chargement du fichier index.html affiché au client
 var server = http.createServer(app);
 var list;
+
 app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "http://10.24.2.41"); // update to match the domain you will make the request from
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*"); // allow requests from any other server
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // allow these verbs
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+    
     next();
 });
-  
-app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-    console.log("hello")
-    res.sendFile(__dirname + 'index.html')
-});
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 app.get('/getSongs', function(req, res) {
-    console.log("getSongs")
-    let jsonData = fs.readFileSync('songList.json')
+    console.log("get songs")
+
+    let jsonData = fs.readFileSync('json/songList.json')
 
     list = JSON.parse(jsonData).songList;
     res.setHeader('Content-Type', 'application/json');
-    res.send(list);
+    res.status(200).send(list);
 })
 
-app.post('/newSong', urlencodedParser, function(req, res) {
-    console.log("new", res)
+app.post('/newSong', [urlencodedParser,jsonParser], function(req, res) {
+    console.log("new song", req.body)
 
-    var newSong = {title: req.body.title, tempo: parseInt(req.body.tempo)};
+    var newSong = {title: req.body.title || "defaultTitle", tempo: parseInt(req.body.tempo || 120), timeSignature: parseInt(req.body.timeSignature || 4)};
+
     newSong.id = Date.now();
     list.push(newSong);    
-    fs.writeFileSync('songList.json', JSON.stringify({songList: list}));
-    res.redirect('/');
+    fs.writeFileSync('json/songList.json', JSON.stringify({songList: list}));
+    res.status(200).send(list);
 })
 
+app.post('/updateSong', [urlencodedParser,jsonParser], function(req, res) {
+    console.log("update song", req.body)
+
+    const {title, id, tempo, timeSignature} = req.body;
+
+    var songtoUpdate = list.find((element) => element.id === id)
+    songtoUpdate.title = title;
+    songtoUpdate.id = id;
+    songtoUpdate.tempo = tempo;
+    songtoUpdate.timeSignature = timeSignature;
+
+    fs.writeFileSync('json/songList.json', JSON.stringify({songList: list}));
+    res.status(200).send(list);
+})
 
 app.post('/deleteSong/:id', urlencodedParser, function(req, res) {
-    console.log("delete", res)
+    console.log("delete song", req.params.id)
+
     var id = parseInt(req.params.id);
     var index = list.findIndex((element) => element.id === id)
     if(index !== -1) {
         list.splice(index, 1);
-        fs.writeFileSync('songList.json', JSON.stringify({songList: list}));
+        fs.writeFileSync('json/songList.json', JSON.stringify({songList: list}));
     }
-    res.redirect(301, '/')
-    
+    res.status(200).send(list);    
 })
 
+app.get('/', function(req, res) {
+    console.log("ok")
+});
 
 
 server.listen(9000);
